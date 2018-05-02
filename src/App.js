2690d1a22bb424b5a6bedcb2a79b1ea93c2158a9
k-yokoishi <ss.xs.713@gmail.com>
2018-05-02 15:46:57 +0900

    feat: Add dependencies for Apollo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { RaisedButton } from 'material-ui';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <RaisedButton label="Login" />
-      </div>
-    );
-  }
-}
+const client = new ApolloClient({
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql'
+});
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(console.log); // disable-eslint no-console
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div className="App">
+      <RaisedButton label="Login" />
+    </div>
+  </ApolloProvider>
+);
 
 export default App;
